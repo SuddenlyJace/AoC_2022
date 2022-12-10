e1603845a84fdfe06453b3e177a29aee36f45e31
root <root@DESKTOP-2VG0QHI.localdomain>
2022-12-10 18:41:05 -0500

    AoC - Day 9 - Part 1 (refactor)

--- 09.py
+++ 09.py
@@ -12,46 +12,45 @@ L 5
 R 2
 '''
 
-def solution(data):
-    head = [0, 0]
-    tail = [0, 0]
+def solution(data, rope_size):
+    rope = [[0, 0] for x in range(rope_size)]
     visited = set()
     
     for move in data:
         # Number of moves
-        for i in range(move[1]):
+        for _ in range(move[1]):
             # Up
             if move[0] == 'U':
-                head[0] += 1
+                rope[0][0] += 1
             # Left
             if move[0] == 'L':
-                head[1] -= 1
+                rope[0][1] -= 1
             # Right
             if move[0] == 'R':
-                head[1] += 1
+                rope[0][1] += 1
             # Down
             if move[0] == 'D':
-                head[0] -= 1
+                rope[0][0] -= 1
 
-            # Adjust tail
-            if abs(head[0] - tail[0]) > 1:
-                if move[0] == 'U':
-                    tail[0] += 1
-                if move[0] == 'D':
-                    tail[0] -= 1
-                tail[1] = head[1]
-            if abs(head[1] - tail[1]) > 1:
-                tail[0] = head[0]
-                if move[0] == 'L':
-                    tail[1] -= 1
-                if move[0] == 'R':
-                    tail[1] += 1
+            # Adjust knots
+            for i in range(1, rope_size):
+                if abs(rope[i-1][0] - rope[i][0]) > 1:
+                    if move[0] == 'U':
+                        rope[i][0] += 1
+                    if move[0] == 'D':
+                        rope[i][0] -= 1
+                    rope[i][1] = rope[i-1][1]
+                if abs(rope[i-1][1] - rope[i][1]) > 1:
+                    rope[i][0] = rope[i-1][0]
+                    if move[0] == 'L':
+                        rope[i][1] -= 1
+                    if move[0] == 'R':
+                        rope[i][1] += 1
 
             # Store tail position in visited
-            visited.add((tail[0], tail[1]))
+            visited.add((rope[rope_size-1][0], rope[rope_size-1][1]))
 
-    solution_1 = len(visited)
-    return solution_1, 0
+    return len(visited)
 
 def parse_input(input):
     input = [x.split(' ') for x in input.splitlines()]
@@ -60,14 +59,17 @@ def parse_input(input):
 
 def main():
     test_data = parse_input(example)
-    solution_1, solution_2 = solution(test_data)
+    solution_1 = solution(test_data, 2)
+    solution_2 = solution(test_data, 10)
     assert(solution_1 == 13)
-    assert(solution_2 == 0)
+    assert(solution_2 == 1)
 
     program_file_name = sys.argv[0]
     input_file_name = 'input_' + os.path.splitext(program_file_name)[0] + '.txt'
     data = parse_input(get_input(input_file_name))
-    solution_1, solution_2 = solution(data)
+    solution_1 = solution(data, 2)
+    assert(solution_1 == 6057)
+    solution_2 = solution(data, 10)
     print('Part 1: ', solution_1)
     print('Part 2: ', solution_2)
     
