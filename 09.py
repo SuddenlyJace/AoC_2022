5dac4f1b1fa6d110e4e38640cfc3639d17e80e98
root <root@DESKTOP-2VG0QHI.localdomain>
2022-12-10 21:17:51 -0500

    AoC - Day 9 - Part 2

--- 09.py
+++ 09.py
@@ -12,6 +12,16 @@ L 5
 R 2
 '''
 
+example2 = '''R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20
+'''
+
 def solution(data, rope_size):
     rope = [[0, 0] for x in range(rope_size)]
     visited = set()
@@ -34,18 +44,15 @@ def solution(data, rope_size):
 
             # Adjust knots
             for i in range(1, rope_size):
-                if abs(rope[i-1][0] - rope[i][0]) > 1:
-                    if move[0] == 'U':
+                if abs(rope[i-1][0] - rope[i][0]) > 1 or abs(rope[i-1][1] - rope[i][1]) > 1:
+                    if rope[i-1][0] > rope[i][0]:
                         rope[i][0] += 1
-                    if move[0] == 'D':
+                    if rope[i-1][0] < rope[i][0]:
                         rope[i][0] -= 1
-                    rope[i][1] = rope[i-1][1]
-                if abs(rope[i-1][1] - rope[i][1]) > 1:
-                    rope[i][0] = rope[i-1][0]
-                    if move[0] == 'L':
-                        rope[i][1] -= 1
-                    if move[0] == 'R':
+                    if rope[i-1][1] > rope[i][1]:
                         rope[i][1] += 1
+                    if rope[i-1][1] < rope[i][1]:
+                        rope[i][1] -= 1
 
             # Store tail position in visited
             visited.add((rope[rope_size-1][0], rope[rope_size-1][1]))
@@ -60,9 +67,12 @@ def parse_input(input):
 def main():
     test_data = parse_input(example)
     solution_1 = solution(test_data, 2)
-    solution_2 = solution(test_data, 10)
     assert(solution_1 == 13)
+    solution_2 = solution(test_data, 10)
     assert(solution_2 == 1)
+    test_data = parse_input(example2)
+    solution_2 = solution(test_data, 10)
+    assert(solution_2 == 36)
 
     program_file_name = sys.argv[0]
     input_file_name = 'input_' + os.path.splitext(program_file_name)[0] + '.txt'
@@ -70,6 +80,7 @@ def main():
     solution_1 = solution(data, 2)
     assert(solution_1 == 6057)
     solution_2 = solution(data, 10)
+    assert(solution_2 == 2514)
     print('Part 1: ', solution_1)
     print('Part 2: ', solution_2)
     
